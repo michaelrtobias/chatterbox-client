411929ee593d1861cdd40800a7eeed238e759148
Michael Tobias <michaelremitobias@gmail.com>
2020-08-28 19:59:10 -0700

    Day 1

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -1,9 +1,10 @@
 class Friends {
   constructor() {
-
   }
 
   //befriending function
-
+  toggleStatus() {
+    // turns friend indicator on when username is clicked on
+  }
 
 }
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -7,7 +7,6 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
-
   },
 
   // grab all rooms.js objects (titles of rooms) and input the names of the rooms into the Room option/selection box
